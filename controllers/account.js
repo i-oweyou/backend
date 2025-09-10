a20d663d7464441cd3c7ed37ca659f2d6d5aaba3
Akbar Jorayev <akbarjonjorayev001@gmail.com>
2025-09-10 10:56:54 +0500

    get user data by username

--- controllers/account.js
+++ controllers/account.js
@@ -48,4 +48,18 @@ async function uniqueUsername(username) {
   }
 }
 
-module.exports = { createAccount, loginAccount }
+async function getDataByUsername(req, res) {
+  const { username } = req.params
+
+  try {
+    const user = await accountServices.getUserByUsername(username)
+    if (!user) return res.status(404).json({ error: 'User not found' })
+
+    delete user.password
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({ error: "It's NOT you, it's US. Sorry" })
+  }
+}
+
+module.exports = { createAccount, loginAccount, getDataByUsername }
--- routes/account.js
+++ routes/account.js
@@ -4,9 +4,15 @@ const {
   validateSignupFields,
   validateLoginFields,
 } = require('../middlewares/account')
-const { createAccount, loginAccount } = require('../controllers/account')
+const {
+  createAccount,
+  loginAccount,
+  getDataByUsername,
+} = require('../controllers/account')
+const { authenticate } = require('../middlewares/auth')
 
 route.post('/signup', validateSignupFields, createAccount)
 route.post('/login', validateLoginFields, loginAccount)
+route.get('/user/:username', authenticate, getDataByUsername)
 
 module.exports = route
